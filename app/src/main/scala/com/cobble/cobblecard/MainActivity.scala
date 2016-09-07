c6b06b4d0bd673796fd5dedb78ebf2b6d6d7e5c2
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-07 16:52:12 -0400

    Initial commit

---
+++ app/src/main/scala/com/cobble/cobblecard/MainActivity.scala
@@ -0,0 +1,13 @@
+package com.cobble.cobblecard
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity extends AppCompatActivity {
+
+    override def onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
