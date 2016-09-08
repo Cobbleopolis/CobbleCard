96c8d75f7eac6edb787734edc2a1994225bbcbf3
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-07 23:21:43 -0400

    Made some changes to CardService

--- app/src/main/scala/com/cobble/cobblecard/CardService.scala
+++ app/src/main/scala/com/cobble/cobblecard/CardService.scala
@@ -1,7 +1,10 @@
 package com.cobble.cobblecard
 
+import android.app.{Notification, NotificationManager, Service}
+import android.content.Context
 import android.nfc.cardemulation.HostApduService
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
 import android.util.Log
 import android.widget.Toast
 
@@ -9,14 +12,30 @@ class CardService extends HostApduService {
 
     val TAG: String = "CardService"
 
+    override def onCreate(): Unit = {
+        super.onCreate()
+
+        Toast.makeText(getApplicationContext, "Service Start", Toast.LENGTH_LONG).show()
+        Log.d(TAG, "Service Started")
+    }
+
     override def processCommandApdu(bytes: Array[Byte], bundle: Bundle): Array[Byte] = {
         val str = "Received apdu: " + byteArrayToHexString(bytes)
         Log.i(TAG, str)
-        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+        Toast.makeText(getApplicationContext, str, Toast.LENGTH_LONG).show()
         Array[Byte](0, 0)
     }
 
-    override def onDeactivated(i: Int): Unit = {}
+    override def onDeactivated(i: Int): Unit = {
+
+    }
+
+    override def onDestroy(): Unit = {
+        super.onDestroy()
+
+        Toast.makeText(getApplicationContext, "Service Stopped", Toast.LENGTH_LONG).show()
+        Log.d(TAG, "Service Stopped")
+    }
 
     def byteArrayToHexString(buf: Array[Byte]): String = buf.map("%02X" format _).mkString
 }
--- app/src/main/scala/com/cobble/cobblecard/MainActivity.scala
+++ app/src/main/scala/com/cobble/cobblecard/MainActivity.scala
@@ -1,13 +1,37 @@
 package com.cobble.cobblecard
 
 
-import android.os.Bundle
+import android.content.{ComponentName, Context, Intent, ServiceConnection}
+import android.os.{Bundle, IBinder}
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity extends AppCompatActivity {
 
-    override def onCreate(savedInstanceState: Bundle) {
+    protected val cardServiceConnection: ServiceConnection = new ServiceConnection {
+
+        val TAG: String = "CARD_SERVICE_CONNECTION"
+
+        override def onServiceDisconnected(name: ComponentName): Unit = {
+            Log.d(TAG, "Card Service Disconnected")
+        }
+
+        override def onServiceConnected(name: ComponentName, service: IBinder): Unit = {
+            Log.d(TAG, "Card Service Connected")
+        }
+    }
+
+    lazy val appContext: Context = getApplicationContext
+    lazy val cardServiceIntent: Intent = new Intent(appContext, classOf[CardService])
+
+    override def onCreate(savedInstanceState: Bundle): Unit = {
         super.onCreate(savedInstanceState)
+        appContext.bindService(cardServiceIntent, cardServiceConnection, Context.BIND_AUTO_CREATE)
         setContentView(R.layout.activity_main)
+    }
+
+    override def onStop(): Unit = {
+        super.onStop()
+        appContext.unbindService(cardServiceConnection)
     }
 }
